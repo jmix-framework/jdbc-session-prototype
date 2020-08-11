ae6025b0fbc679c1bb2848e26e55a06fb6704956
zaharchenko <zaharchenko@haulmont.com>
2020-08-11 14:57:58 +0400

    Rest tests

--- src/main/java/org/example/jdbcsession/SampleRestAuthorizationServerConfiguration.java
+++ src/main/java/org/example/jdbcsession/SampleRestAuthorizationServerConfiguration.java
@@ -1,11 +1,8 @@
 package org.example.jdbcsession;
 
-import org.example.jdbcsession.rest.SessionTokenEnhancer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.AuthenticationKeyGenerator;
@@ -23,20 +20,11 @@ public class SampleRestAuthorizationServerConfiguration extends AuthorizationSer
     @Autowired
     protected AuthenticationKeyGenerator keyGenerator;
 
-//    @Autowired
-//    protected AuthenticationManager authenticationManager;
-
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
         endpoints.tokenStore(tokenStore());
     }
 
-//    @Bean
-//    public SessionTokenEnhancer tokenEnhancer() {
-//        return new SessionTokenEnhancer();
-//    }
-
-//    @Primary
     @Bean
     protected TokenStore tokenStore() {
         JdbcTokenStore tokenStore = new JdbcTokenStore(dataSource);
--- src/test/java/org/example/jdbcsession/SampleApplicationTests.java
+++ src/test/java/org/example/jdbcsession/SampleApplicationTests.java
@@ -1,53 +1,104 @@
 package org.example.jdbcsession;
 
 import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.example.jdbcsession.controller.SessionRestController;
-import org.junit.Assert;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.context.WebApplicationContext;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest(classes = SampleApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SampleApplicationTests {
 
     protected static MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(),
             StandardCharsets.UTF_8);
 
+    private final static String ACCESS_TOKEN_KEY = "access_token";
+    private final static String REFRESH_TOKEN_KEY = "refresh_token";
+
     @Autowired
     private MockMvc mockMvc;
 
+    protected static String persistentOAuth;
+    protected static String persistentRefreshToken;
+    protected static String persistentAttributeValue;
+
     @Test
+    @Order(0)
+    void predefinedAttributes() throws Exception {
+        Map<String, Object> tokenMap = obtainAccessToken("admin", "admin");
+        persistentOAuth = tokenMap.get(ACCESS_TOKEN_KEY).toString();
+        persistentRefreshToken = tokenMap.get(REFRESH_TOKEN_KEY).toString();
+
+        persistentAttributeValue = RandomStringUtils.randomAlphabetic(10);
+        assertNotNull(persistentOAuth);
+
+        mockMvc.perform(
+                auth(persistentOAuth, put(SessionRestController.API_URL)
+                        .param("name", "test")
+                        .param("value", persistentAttributeValue)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Order(1)
+    void persistentStorageTest() throws Exception {
+        String resultValue = mockMvc.perform(
+                auth(persistentOAuth, get(SessionRestController.API_URL)
+                        .param("name", "test")))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        assertEquals(persistentAttributeValue, resultValue);
+
+    }
+
+    @Test
+    @Order(2)
+    void useRefreshToken() throws Exception {
+        Map<String, Object> tokenMap = obtainAccessToken(persistentRefreshToken);
+        persistentOAuth = tokenMap.get(ACCESS_TOKEN_KEY).toString();
+        persistentRefreshToken = tokenMap.get(REFRESH_TOKEN_KEY).toString();
+
+        String resultValue = mockMvc.perform(
+                auth(persistentOAuth, get(SessionRestController.API_URL)
+                        .param("name", "test")))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        assertEquals(persistentAttributeValue, resultValue);
+
+    }
+
+    @Test
+    @Order(3)
     void userSessionTest() throws Exception {
-        String oAuth = obtainAccessToken("admin", "admin");
+        String oAuth = obtainAccessToken("admin", "admin").get(ACCESS_TOKEN_KEY).toString();
         String attributeValue = RandomStringUtils.randomAlphabetic(10);
 
         mockMvc.perform(
@@ -75,25 +126,13 @@ class SampleApplicationTests {
                 .andExpect(status().isUnauthorized());
     }
 
-    @Test
-    void persistentStorageTest() throws Exception {
-        String resultValue = mockMvc.perform(
-                auth("Zr55uo9QJQuRz5k+tFOXbeFQut0=", get(SessionRestController.API_URL)
-                        .param("name", "test")))
-                .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
-
-        assertEquals("gvxxCsRWzQ", resultValue);
-
-    }
-
     private static MockHttpServletRequestBuilder auth(String oAuth, MockHttpServletRequestBuilder requestBuilder) {
         return requestBuilder.accept(contentType)
                 .header("Authorization", "Bearer " + oAuth)
                 .header("Accept-Language", "en");
     }
 
-    public String obtainAccessToken(String username, String password) throws Exception {
+    public Map<String, Object> obtainAccessToken(String username, String password) throws Exception {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "password");
         params.add("username", username);
@@ -107,7 +146,23 @@ class SampleApplicationTests {
                 .andExpect(content().contentType(contentType));
         String resultString = result.andReturn().getResponse().getContentAsString();
 
-        return new JacksonJsonParser().parseMap(resultString).get("access_token").toString();
+        return new JacksonJsonParser().parseMap(resultString);
+    }
+
+    public Map<String, Object> obtainAccessToken(String refreshToken) throws Exception {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "refresh_token");
+        params.add("refresh_token", refreshToken);
+
+        ResultActions result = mockMvc.perform(post("/rest/oauth/token")
+                .params(params)
+                .with(httpBasic("client", "secret"))
+                .accept(contentType))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType));
+        String resultString = result.andReturn().getResponse().getContentAsString();
+
+        return new JacksonJsonParser().parseMap(resultString);
     }
 
 }
